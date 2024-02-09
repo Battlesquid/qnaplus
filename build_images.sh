68e607e866f5da8ad466cf67be67b96eda483fcd
Battlesquid <esanoluwamayowa@gmail.com>
2024-02-08 23:44:22 -0700

    fix: fix build step

--- build_images.sh
+++ build_images.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build --rm -t qnaplus/bot -f ./services/bot/Dockerfile .
-docker build --rm -t qnaplus/updater -f ./services/updater/Dockerfile .
+docker build --rm -t battlesquid0101/qnaplus_bot -f ./services/bot/Dockerfile .
+docker build --rm -t battlesquid0101/qnaplus_updater -f ./services/updater/Dockerfile .
---
+++ push_images.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker push battlesquid0101/qnaplus_updater:latest
+docker push battlesquid0101/qnaplus_bot:latest
