13fd90cb9e8e5ddc8df458ec8f0484dd8da652e7
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2024-06-05 09:24:09 -0700

    fix: switch to croner

--- services/updater/src/index.ts
+++ services/updater/src/index.ts
@@ -1,12 +1,12 @@
 import { getLoggerInstance } from "@qnaplus/logger";
-import { schedule } from "node-cron";
+import Cron from "croner";
 import { config, update } from "qnaplus";
 
 (async () => {
     const logger = getLoggerInstance("qnaupdater");
     logger.info("Starting updater service");
     await update(logger);
-    schedule(config.getenv("UPDATE_INTERVAL"), () => {
+    Cron(config.getenv("UPDATE_INTERVAL"), () => {
         update(logger);
     });
 })();
