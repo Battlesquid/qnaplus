0b9581f4bada2eb9b5f2785a3f8f198bb4735a7e
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2024-06-18 09:00:53 -0700

    feat: add pwa functionality

--- services/web/vite.config.ts
+++ services/web/vite.config.ts
@@ -1,10 +1,40 @@
 import vue from '@vitejs/plugin-vue';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
     plugins: [
-        vue()
+        vue(),
+        VitePWA({
+            registerType: 'prompt',
+            injectRegister: "inline",
+
+            pwaAssets: {
+                disabled: false,
+                config: true,
+            },
+
+            manifest: {
+                name: 'qnaplus',
+                short_name: 'qnaplus',
+                description: 'Advanced search tool for the VEX Robotics Q&A',
+                theme_color: '#1a1b26',
+            },
+
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+            },
+
+            devOptions: {
+                enabled: false,
+                navigateFallback: 'index.html',
+                suppressWarnings: true,
+                type: 'module',
+            },
+        })
     ],
 })
