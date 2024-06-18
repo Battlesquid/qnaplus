5eddf423e1805a7c3a81490d403e29ef1eea9c77
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2024-06-18 10:45:52 -0700

    fix: improved responsiveness for filters accordion

--- services/web/src/components/SearchFilters.vue
+++ services/web/src/components/SearchFilters.vue
@@ -2,7 +2,6 @@
 import Button from 'primevue/button';
 import Calendar from "primevue/calendar";
 import Chips from "primevue/chips";
-import Divider from 'primevue/divider';
 import IconField from "primevue/iconfield";
 import InputGroup from "primevue/inputgroup";
 import InputGroupAddon from 'primevue/inputgroupaddon';
@@ -25,31 +24,39 @@ defineProps<{
     <Panel header="Filters" toggleable collapsed>
         <div class="flex flex-column gap-3">
             <div class="flex flex-wrap gap-2">
-                <div class="flex flex-column gap-1">
-                    <label for="season">Season</label>
-                    <MultiSelect id="season" v-model="filters.season" placeholder="Season" :options="seasons"
-                        option-label="name" />
+
+                <div class="flex gap-2 w-full">
+                    <div class="flex flex-column flex-grow-1 gap-1">
+                        <label for="season">Season</label>
+                        <MultiSelect id="season" v-model="filters.season" placeholder="Season" :options="seasons"
+                            option-label="name" />
+                    </div>
+                    <div class="flex flex-column flex-grow-1 gap-1">
+                        <label for="program">Program</label>
+                        <MultiSelect id="program" v-model="filters.program" placeholder="Program" :options="programs"
+                            option-label="name" />
+                    </div>
                 </div>
-                <div class="flex flex-column gap-1">
-                    <label for="program">Program</label>
-                    <MultiSelect id="program" v-model="filters.program" placeholder="Program" :options="programs"
-                        option-label="name" />
-                </div>
-                <div class="flex flex-1 flex-column gap-1">
-                    <label for="author">Author</label>
-                    <IconField class="flex-1" icon-position="left">
-                        <i class="pi pi-user"></i>
-                        <InputText id="author" class="w-full" v-model="filters.author" placeholder="Author" />
-                    </IconField>
-                </div>
-                <div class="flex flex-column gap-1">
-                    <label for="Question State">Question State</label>
-                    <SelectButton :allow-empty="false" v-model="filters.state" :options="options" option-label="name"
-                        aria-labelledby="multiple" />
+
+                <div class="flex flex-wrap gap-2 w-full">
+                    <div class="flex flex-grow-1 flex-column gap-1">
+                        <label for="author">Author</label>
+                        <IconField class="flex-1" icon-position="left">
+                            <i class="pi pi-user"></i>
+                            <InputText id="author" class="w-full" v-model="filters.author" placeholder="Author" />
+                        </IconField>
+                    </div>
+
+                    <div class="flex flex-column flex-grow-1 gap-1">
+                        <label for="Question State">Question State</label>
+                        <SelectButton class="select-button-flex" :allow-empty="false" v-model="filters.state"
+                            :options="options" option-label="name" aria-labelledby="multiple" />
+                    </div>
                 </div>
             </div>
-            <div class="flex">
-                <div class="flex flex-1 flex-1 gap-2">
+
+            <div class="flex flex-wrap gap-2 ">
+                <div class="flex flex-1 flex-1 min-w-fit gap-2">
                     <div class="flex flex-column flex-1 gap-1">
                         <label for="askedBefore">Asked Before</label>
                         <Calendar id="askedBefore" v-model="filters.askedBefore" placeholder="Asked Before" show-icon
@@ -61,8 +68,7 @@ defineProps<{
                             show-button-bar icon-display="input" />
                     </div>
                 </div>
-                <Divider layout="vertical" />
-                <div class="flex flex-1 gap-2">
+                <div class="flex flex-1 min-w-fit gap-2">
                     <div class="flex flex-column flex-1 gap-1">
                         <label for="answeredBefore" aria-label="Answered Before">Answered Before</label>
                         <Calendar id="answeredBefore" v-model="filters.answeredBefore" placeholder="Answered Before"
@@ -91,4 +97,12 @@ defineProps<{
     </Panel>
 </template>
 
-<style scoped></style>
+<style scoped>
+.min-w-fit {
+    min-width: fit-content;
+}
+
+.select-button-flex {
+    display: flex;
+}
+</style>
